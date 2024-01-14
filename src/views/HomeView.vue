0c28e29f2d3595463cfcbb97c1e8b58f466b6ca9
biQte <patrykgonet@gmail.com>
2024-01-14 18:43:21 +0100

    Try to boost performance

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import TheMenu from '../components/TheMenu.vue';
 
 <template>
     <div class="home-view">
-        <img src="/public/intel-logo.svg" alt="intel-logo" class="intel-logo" />
+        <img src="/intel-logo.svg" alt="intel-logo" class="intel-logo" />
         <h1>Witaj!</h1>
         <h3>Tutaj powstaje symulator procesora Intel 8086</h3>
         <TheMenu />
